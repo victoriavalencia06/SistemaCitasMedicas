250f494853fe057e69e802dd0bc9350df95f5122
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-10-06 15:14:49 -0600

    Creacion de la capa de pruebas unitarias

---
+++ SistemaCitasMedicas.UnitTest.AppMSTest/Services/RolServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaCitasMedicas.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCitasMedicas.Application.Services.Tests
+{
+    [TestClass()]
+    public class RolServiceTests
+    {
+        [TestMethod()]
+        public void RolServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerRolPorIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarRolAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerRolesActivosAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AgregarRolAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
